f82103e4c2bfe45d251175968821ddb7663474a2
Rafael Joia <rafael.joia@fgv.br>
2024-04-22 12:08:18 -0300

    adapter folder to commands

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite',
-    'render'
-]
+  ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
