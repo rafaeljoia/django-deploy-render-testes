3d94b2ff945871fad5bd79074476acb0f687ea53
Rafael Joia <rafael.joia@fgv.br>
2024-06-06 16:15:25 -0300

    Add scrapy

--- render/models.py
+++ render/models.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 
 class Imovel(models.Model):
     TIPO_CHOICES = [
@@ -13,7 +16,7 @@ class Imovel(models.Model):
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     tipo = models.CharField(max_length=50, choices=TIPO_CHOICES)
     imobiliaria = models.CharField(max_length=100, default="DeFranco")
-    current_date = datetime.date.today()
+    current_date = models.DateField(auto_now_add=True)  # Use auto_now_add para definir a data automaticamente no momento da criação do objeto
 
     def __str__(self):
         return f"{self.tipo} em {self.bairro} - R$ {self.valor}"
@@ -22,3 +25,10 @@ class Imovel(models.Model):
         verbose_name = "Imóvel"
         verbose_name_plural = "Imóveis"
         ordering = ['bairro', 'valor']
+
+
+# Signal para definir a data atual antes de salvar o objeto Imovel
+@receiver(pre_save, sender=Imovel)
+def set_current_date(sender, instance, *args, **kwargs):
+    if not instance.pk:  # Verifica se o objeto ainda não foi salvo no banco de dados
+        instance.current_date = datetime.now().date()  # Define a data atual apenas quando o objeto está sendo criado
