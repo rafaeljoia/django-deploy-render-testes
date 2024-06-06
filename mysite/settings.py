ec235ed3f21aa0947810cd532fff41124cf50cdd
Rafael Joia <rafael.joia@fgv.br>
2024-06-06 17:12:46 -0300

    Change settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,7 +35,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'render.apps.RenderConfig',
+    'render',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
